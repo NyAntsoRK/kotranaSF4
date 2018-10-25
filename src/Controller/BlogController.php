c94940caf267ffb5724b87aabb5468f61f7aabe8
NyAntso <nyantsorakotondrabe@gmail.com>
2018-10-25 22:40:22 +0300

    Formbuilder design

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -4,7 +4,12 @@ namespace App\Controller;
 
 
 use App\Repository\ArticleRepository;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Article;
 
@@ -37,8 +42,43 @@ class BlogController extends AbstractController
     }
 
     /**
+     * @route ("/blog/new", name="blog_new")
+     */
+
+    public function addArticle(Request $request, ObjectManager $manager){
+
+        $article = new Article();
+
+        $form    = $this->createFormBuilder($article)
+                        ->add('title', TextType::class,[
+                            'attr' => [
+                                'placeholder' => "Titre de l'article"
+                            ]
+                        ])
+
+                        ->add('content', TextareaType::class,[
+                            'attr' => [
+                                'placeholder' => "Contenu de l'article"
+                            ]
+                        ])
+
+                        ->add('image', TextType::class,[
+                            'attr' => [
+                                'placeholder' => "Image de l'article"
+                            ]
+                        ])
+                        ->getForm();
+
+        return $this->render('blog/add.html.twig',[
+
+            'formArticle' => $form->createView()
+        ]);
+    }
+
+    /**
      * @route ("/blog/{id}", name="blog_show")
      */
+
     public function show(Article $article){
 
        // $rep = $this->getDoctrine()->getRepository(Article::class);
@@ -48,8 +88,5 @@ class BlogController extends AbstractController
         ]);
     }
 
-    public function addArticle(){
 
-        return $this->render('blog/add.html.twig');
-    }
 }
